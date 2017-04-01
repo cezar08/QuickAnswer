cfd6457ae3d9fb1060f695aa1d9dfad98fefd2c7
WesleySartori <wesleysartori@unochapeco.edu.br>
2017-03-31 21:43:01 -0300

    SalaValidator Ilso/Wesley

--- module/Application/src/Validator/SalaValidator.php
+++ module/Application/src/Validator/SalaValidator.php
@@ -3,7 +3,6 @@
  * Created by PhpStorm.
  * User: Ilso Pasa
  * Date: 31/03/17
- * Time: 20:54
  */
 
 namespace Application\Validator;
@@ -17,7 +16,7 @@ class SalaValidator extends InputFilter
     {
         $factory = new InputFactory();
         $this->add(
-            $factory->CreateInput(
+            $factory->createInput(
                 [
                     'name' => 'id',
                     'required' => 'false',
@@ -34,12 +33,98 @@ class SalaValidator extends InputFilter
         $this->add(
             $factory->createInput(
                 [
-                    'name' => ''
+                    'name' => 'nomeSala',
+                    'required' => true,
+                    'filters' => [
+                        ['name' => 'StringTags'],
+                        ['name' => 'StringTrim']
+                    ],
+                    'validator' => [
+                        [
+                        'name' => 'StringLength',
+                        'options' => [
+                            'encoding' => 'UTF-8',
+                            'min' => 3,
+                            'max' => 60
+                            ]
+                        ]
+                    ],
                 ]
             )
-        )
+        );
 
+        $this->add(
+            $factory->createInput(
+                [
+                    'name' => 'dataCriacao',
+                    'required' => false,
+                    'validators' => [
+                        [
+                            'name' => 'dataCriacao',
+                            'options' => [
+                                'format' => 'd/m/Y'
+                            ]
+                        ]
+                    ]
+                ]
+            )
+        );
+
+        $this->add(
+            $factory->createInput(
+                [
+                   'name' => 'tipo',
+                   'required' => true,
+                   'validators' => [
+                       [
+                           'name' => 'InArray',
+                           'options' => [
+                               'haystack' => ['Publica', 'Privada']
+                           ]
+                       ]
+                   ]
+                ]
+            )
+        );
+
+        $this->add(
+            $factory->createInput(
+                [
+                    'name' => 'usuario',
+                    'required' => true,
+                    'filters' => [
+                        ['name' => 'StringTags'],
+                        ['name' => 'StringTrim']
+                    ],
+                    'validator' => [
+                        [
+                            'name' => 'StringLength',
+                            'options' => [
+                                'encoding' => 'UTF-8',
+                                'min' => 3,
+                                'max' => 60
+                            ]
+                        ]
+                    ],
+                ]
+            )
+        );
+
+        $this->add(
+            $factory->createInput(
+                [
+                    'name' => 'perguntas',
+                    'required' => true,
+                    'validators' => [
+                        [
+                            'name' => 'InArray',
+                            'options' => [
+                                'haystack' => ['Qual é a materia que te faz aprender ZendFramework na marra ?']
+                            ]
+                        ]
+                    ]
+                ]
+            )
+        );
     }
-
-
 }
