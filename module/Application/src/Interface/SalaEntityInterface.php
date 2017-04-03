275b8f976522c357f1c820c338d214680caa0264
Jean <jcdacampo@unochapeco.edu.br>
2017-04-03 20:17:23 -0300

    Interface

---
+++ module/Application/src/Interface/SalaEntityInterface.php
@@ -0,0 +1,13 @@
+<?php 
+namespace Zend\Mvc;
+
+interface SalaInterface
+{
+    // Método para buscar uma sala
+    public function buscaSala();
+
+    // Método para definir se a sala é privada ou pública
+    public function definePerfilSala();
+
+}
+
