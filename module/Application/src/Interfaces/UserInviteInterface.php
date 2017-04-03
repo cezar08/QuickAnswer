3baca382d4befc64c3dbbc4c6584740b248fdc08
darkmush12 <weiler.cavasotto@unochapeco.edu.br>
2017-04-03 20:19:51 -0300

    Nome refatorado

--- module/Application/src/Interfaces/UserInviteInterface.php
+++ module/Application/src/Interfaces/UserInviteInterface.php
@@ -1,15 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: unochapeco
- * Date: 03/04/17
- * Time: 20:04
- */
 
 namespace Application\Interfaces;
 
-
-interface UserInviteInterface
+interface UserInviteEntityInterface
 {
 
 }
