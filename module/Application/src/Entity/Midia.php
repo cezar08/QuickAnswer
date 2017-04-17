62448892cd41e411e6e4add8f5031cbff1625c69
rafamp1991 <rafael.padua@unochapeco.edu.br>
2017-04-17 19:24:58 -0300

    Entity

---
+++ module/Application/src/Entity/Midia.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Application\Entity;
+use Application\Interfaces\MidiaInterface;
+
+class Midia extends Entity implements MidiaInterface
+{
+    protected $id;
+
+    protected $typeofmidia;
+
+    protected $description;
+
+    protected $data;
+
+    protected $path;
+
+}
