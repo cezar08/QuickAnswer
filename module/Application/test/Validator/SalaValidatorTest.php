adc7a5a8cecc4b293ec2005a6940691e64c5b66b
WesleySartori <wesleysartori@unochapeco.edu.br>
2017-04-24 19:27:31 -0300

    Atualização 24/04

--- module/Application/test/Validator/SalaValidatorTest.php
+++ module/Application/test/Validator/SalaValidatorTest.php
@@ -8,6 +8,7 @@
 
 namespace Application\src\Validator;
 
+use Application\Entity\SalaEntity;
 use PHPUnit\Framework\TestCase;
 use Application\src\Validator\SalaValidator;
 
@@ -16,59 +17,55 @@ class SalaValidatorTest extends ModelTestCase
     /**
      * Herança
      *
-     * Variavel $aux contendo a variavel $ramo_de_negocio_validator
+     * Variavel $aux contendo a variavel $salavalidator
      * para execuçao no codesniffer
      *
      * @return void
      */
     public function testInherit()
     {
-        $ramo_de_negocio_validator = new RamoDeNegocioValidator();
-        $aux = $ramo_de_negocio_validator;
+        $salaValidator = new SalaValidator();
+        $aux = $salaValidator;
         $this->assertInstanceOf('Zend\InputFilter\InputFilter', $aux);
     }
 
     /**
-     * Teste de campos
+     * Teste dos Campos
      *
      * @return void
      */
     public function testFields()
     {
-        $ramo_de_negocio = new RamoDeNegocio();
-        $ramo_de_negocio_validator = new RamoDeNegocioValidator();
-        $ramo_de_negocio_array_copy = $ramo_de_negocio->getArrayCopy();
-        $ramo_de_negocio_fields = array_keys($ramo_de_negocio_array_copy);
+        //$salaEntity = new SalaEntity();
+        $salaValidator = new SalaValidator();
+        $salaValidatorArrayCopy = $salaValidator->getArrayCopy();
+        $salaValidatorFields = array_key($salaValidatorArrayCopy);
 
-        foreach ($ramo_de_negocio_fields as $field) {
-
-            if (!$ramo_de_negocio_validator->has($field)) {
-                $this->fail($field . 'não existe na validação');
+        foreach ($salaValidatorFields as $field) {
+            if (!$salaValidator->has($field)) {
+                $this->fail($field . 'validação não existente! ');
             }
         }
-
-        $this->assertEquals(3, $ramo_de_negocio_validator->count());
-        $this->assertCount(3, $ramo_de_negocio_fields);
+        $this->assertEquals(6, $salaValidator->count);
+        $this->assertCount(6, $salaValidatorFields);
     }
 
     /**
      * Teste de filtros
      *
-     * Variavel $mensagem contem a descrição do ramo
+     * Variavel $mensagem contem a descrição
      *
      * @return void
      */
     public function testFilters()
     {
-        $ramo_de_negocio_validator = new RamoDeNegocioValidator();
-        $mensagem = '<a href="www.google.com.br">Ramo Industria</a>';
-        $dados = array(
-            'id' => 'nao pode ser string', 'desc_ramo_de_negocio' => $mensagem
-        );
-        $ramo_de_negocio_validator->setData($dados);
-        $this->assertTrue($ramo_de_negocio_validator->isValid());
-        $this->assertEquals(0, $ramo_de_negocio_validator->get('id')->getValue());
-        $desc = $ramo_de_negocio_validator->get('desc_ramo_de_negocio')->getValue();
-        $this->assertEquals('RAMO INDUSTRIA', $desc);
+        $salaValidator = new SalaValidator();
+        $mensagem = '<a href="www.google.com.br">Sala App</a>';
+        $dados = array('id' => 'nao pode ser string', 'sem_validacao' => $mensagem);
+        $salaValidator->setData($dados);
+        $this->assertTrue($salaValidator->isValid());
+        $this->assertEquals(0, $salaValidator->get('id')->getValue());
+        $sala = $salaValidator->get('sala_app')->getValue();
+        $this->assertEquals('SALA APP', $sala);
     }
 }
