37c3715764de2413f9013be4372fcc1344a6f62a
brunofurtadofontana <brunofurtado86@hotmail.com>
2017-04-24 20:04:47 -0300

    Service Choice

---
+++ module/Application/src/Service/ChoiceService.php
@@ -0,0 +1,18 @@
+<?php
+namespace Application\Service;
+
+use Doctrine\ORM\EntityManager;
+use Zend\Validator\EmailAddress;
+use Zend\Validator\StringLength;
+    class ChoiceService{
+    // protected $entityManger;
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManger = $entityManager;
+    }
+    public function dataBaseAuth($data)
+    {
+
+        }
+    }
+?>
