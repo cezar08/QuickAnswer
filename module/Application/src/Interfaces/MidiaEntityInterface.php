caf9843a7a5f37dd0c089a4546d4381af08f32c2
rafamp1991 <rafael.padua@unochapeco.edu.br>
2017-05-29 21:23:50 -0300

    MidiaEntityInterface revisada.

--- module/Application/src/Interfaces/MidiaEntityInterface.php
+++ module/Application/src/Interfaces/MidiaEntityInterface.php
@@ -1,13 +1,53 @@
 <?php
-
 namespace Application\Entity;
-use Application\Interfaces\MidiaEntityInterface;
 
+use Application\Interfaces\MidiaEntityInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class MidiaEntity
+ * @package Application\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="midia_qa")
+ */
 class MidiaEntity extends Entity implements MidiaEntityInterface
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type = "integer")
+     *
+     * @var int
+     */
     protected $_id;
-    protected $_typeofmidia;
+
+    /**
+     * @ORM\TypeOfMidia
+     * @ORM\Column(type = "integer")
+     *
+     * @var int
+     */
+    protected $_typeOfMidia;
+
+    /**
+     * @ORM\Column(type="string", length=60, nullable=false)
+     *
+     * @var string
+     */
     protected $_description;
+
+    /**
+     * @ORM\Column(type="date", nullable=false)
+     *
+     * @var date
+     */
     protected $_dateMidia;
+
+    /**
+     * @ORM\Column(type="string", length=120, nullable=false)
+     *
+     * @var string
+     */
     protected $_path;
 }
