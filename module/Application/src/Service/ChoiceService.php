8b066a60476a49a023ffc120cd8e6eb5b34a2228
brunofurtadofontana <brunofurtado86@hotmail.com>
2017-06-09 13:43:57 -0300

    choiceService

--- module/Application/src/Service/ChoiceService.php
+++ module/Application/src/Service/ChoiceService.php
@@ -1,18 +1,46 @@
 <?php
 namespace Application\Service;
 
-use Doctrine\ORM\EntityManager;
-use Zend\Validator\EmailAddress;
-use Zend\Validator\StringLength;
-    class ChoiceService{
+use Application\Entity\ChoiceEntity;
+use Application\Entity\QuestionEntity;
+use Application\Entity\TypeMutipleOptionsEntity;
+use Application\Entity\TypeQuickAnswer;
+use Application\Validator\QuestionValidator;
+
+class ChoiceService{
     protected $entityManger;
-    public function __construct(EntityManager $entityManager)
-    {
-        $this->entityManger = $entityManager;
-    }
-    public function dataBaseAuth($data)
-    {
+    public function __construct(EntityManager $entityManager){
+    $this->entityManger = $entityManager;
+}
+    public function saveChoice($values){
+        $validate = new ChoiceValidator();
+        $validate->setData($values);
+
+        if (!$validate->isValid())
+        return $validate->getMessages();
+
+        $em = $this->getEntityManager();
+        $choice = new ChoiceEntity();
+
+        if (isset($values['description'])) {
+            $descChoice = new ChoiceEntity();
+            $descChoice = $this->setDescription($descChoice, $values['description']);
+        } else {
 
         }
+        if (isset($values['correct'])) {
+            $correctChoice = new ChoiceEntity();
+            $correctChoice = $this->setCorrect($correctChoice, $values['correct']);
+        } else {
+
+        }
+
+        $choice->__set("description", $values['description']);
+        $choice->__set("correct", $values['correct']);
+        $em->persist($choice);
+        $em->flush();
     }
+    }
+
+}
 ?>
