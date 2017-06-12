966deb3fa7ca6ab5b33c7c7842e1b4880f067199
rafamp1991 <rafael.padua@unochapeco.edu.br>
2017-06-12 20:33:58 -0300

    arquivos revisados

--- module/Application/src/Controller/MediaController.php
+++ module/Application/src/Controller/MediaController.php
@@ -2,9 +2,7 @@
 
 namespace Application\Controller;
 
-
 use Zend\Mvc\Controller\AbstractActionController;
-use Application\Entity\MediaEntity;
 
 class MediaController extends AbstractActionController
 {
@@ -15,9 +13,7 @@ class MediaController extends AbstractActionController
     public function storeMediaAction()
     {
         try {
-
             if ($this->getRequest()->isPost()) {
-
                 $dateOfNow = new \DateTime();
                     $date = $dateOfNow->format('d_m_Y_H:i:s_');
                     $move_media = getcwd()."/public/img/media_repository/" .
@@ -27,15 +23,52 @@ class MediaController extends AbstractActionController
 
                     move_uploaded_file($_FILES['media']['tmp_name'], $move_media);
 
-                    $media_type = 'image';
+                    $ext = pathinfo($_FILES['media']['name'], PATHINFO_EXTENSION);
+
+                    $mime_type_image = [
+
+                        // image
+                        'png' => 'image/png',
+                        'jpe' => 'image/jpeg',
+                        'jpeg' => 'image/jpeg',
+                        'jpg' => 'image/jpeg',
+                        'gif' => 'image/gif',
+                        'bmp' => 'image/bmp',
+                        'ico' => 'image/vnd.microsoft.icon',
+                        'tiff' => 'image/tiff',
+                        'tif' => 'image/tiff',
+                        'svg' => 'image/svg+xml',
+                        'svgz' => 'image/svg+xml',
+                    ];
+
+                    $mime_type_audio = [
+
+                        // audio
+                        'mp3' => 'audio/mpeg',
+                    ];
+
+                    $mime_type_video = [
+
+                        // video
+                        'qt' => 'video/quicktime',
+                        'mov' => 'video/quicktime',
+                    ];
+
+                    if (array_key_exists($ext, $mime_type_image)) {
+                        $media_type = 'image';
+                    } elseif (array_key_exists($ext, $mime_type_audio)) {
+                        $media_type = 'audio';
+                    } elseif (array_key_exists($ext, $mime_type_video)) {
+                        $media_type = 'video';
+                    } else {
+                        return 'Extension Invalid';
+                    }
 
                     $this->getService('MediaService')
                         ->persistMedia($media_type, $media_path);
             }
-
         } catch (\Exception $e) {
-
-            return array ('error' => $e);
+            return ['error' => $e];
         }
     }
 }
--- module/Application/test/Service/MediaServiceTest.php
+++ module/Application/test/Service/MediaServiceTest.php
@@ -1,21 +1,17 @@
 <?php
-/**
- * @author Rafael/Alessandro
- */
+
 namespace ApplicationTest\Service;
 
-use Application\Entity\MediaEntity;
 use Application\MediaService;
-use PHPUnit\Framework\TestCase;
 
 /**
  * Class MediaServiceTest
  * @package ApplicationTest\Service
  * @group Service
  */
-class MediaServiceTest extends AbstractServiceTestCase
+class MediaServiceTest extends \ApplicationTest\Service\AbstractServiceTestCase
 {
-	/**
+    /**
      * Test of insertion of a media.
      */
     public function testPersistMedia()
