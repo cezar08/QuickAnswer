f1a80e613c5761a6d065e61fb7571b08070a4fa5
Marcos <merlinimarcos@gmail.com>
2017-06-12 21:35:43 -0300

    Teste Hash Service

--- module/Application/test/Service/HashServiceTest.php
+++ module/Application/test/Service/HashServiceTest.php
@@ -1,15 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: unochapeco
- * Date: 12/06/17
- * Time: 21:29
- */
 
 namespace ApplicationTest\Service;
 
 
-class HashServiceTest
+use Application\Service\HashService;
+
+/**
+ * Class HashServiceTest
+ * @package ApplicationTest\Service
+ * @group Service
+ */
+class HashServiceTest extends AbstractServiceTestCase
+
 {
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function testUniqueURL()
+    {
+        $service = $this->getApplicationServiceLocator()
+            ->get('HashService');
+        $url1 = $service->returnHash();
+        $url2 = $service->returnHash();
+        $this->assertNotEquals($url1, $url2);
+    }
+
+    public function testQtdCharacteres()
+    {
+        $service = $this->getApplicationServiceLocator()
+            ->get('HashService');
+        $url = $service->returnHash();
+        $this->assertEquals(32, strlen($url));
+    }
+
 
 }
